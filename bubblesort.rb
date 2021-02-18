bea3764e43eec1ddeaf9348bf8fbf8ada79a4100
promise-J <chiemelapromise30@gmail.com>
2021-02-19 01:05:31 +1300

    Fix linters errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,15 @@
 def bubble_sort(array)
 #  using generic loop for the project --bubble_sort
-array_size = array.length
+  array_size = array.length
 loop do
-
-    did_swap = false
+  did_swap = false
   (array_size-1).times do |i|
     if array[i] > array[i + 1]
-        array[i], array[i+1] = array[i+1], array[i]
-    did_swap = true
+      array[i], array[i+1] = array[i+1], array[i]
+  did_swap = true
     end
   end
-  break if not did_swap
+  break if !did_swap
   end
   array
 end
@@ -20,20 +19,18 @@ end
 # second project -- bubble_sort_by
 
 def bubble_sort_by(arr)
-     array_size = arr.length - 1
-    array_size.times do |i|
-        array_size.times do |j|
-          if yield(arr[j], arr[j+1]).positive?
-            arr[j], arr[j+1] = arr[j+1], arr[j]
-          end
-        end
+  array_size = arr.length - 1
+  array_size.times do |i|
+  array_size.times do |j|
+    if yield(arr[j], arr[j+1]).positive?
+    arr[j], arr[j+1] = arr[j+1], arr[j]
     end
+  end
+  end
   p arr
 end
 
-
-
- bubble_sort_by(["hi", "hello", "hey"]) do |first, second|
-  first.length - second.length
+bubble_sort_by(["hi", "hello", "hey"]) do |first, second|
+first.length - second.length
 end
 # bubble_sort_by(arr)
