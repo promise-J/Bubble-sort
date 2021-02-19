134e7897c22fe6a10dfac322b2f8ba8b275e31f2
promise-J <chiemelapromise30@gmail.com>
2021-02-19 16:40:01 +1300

    Attempt fix on linter error

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,30 +1,32 @@
 def bubble_sort(array)
-	array_size = array.length
-	loop do
-		did_swap = false
-		(array_size - 1).times do |i|
-			if array[i] > array[i + 1]
-				array[i], array[i + 1] = array[i + 1], array[i]
-				did_swap = true
-			end
+    array_size = array.length
+	  loop do
+		  did_swap = false
+		    (array_size - 1).times do |i|
+			    if array[i] > array[i + 1]
+				    array[i], array[i + 1] = array[i + 1], array[i]
+				    did_swap = true
+			    end
 		end
 		break unless did_swap
-	end
+	  end
 	array
 end
-p bubble_sort()
+# p bubble_sort
+
+
 def bubble_sort_by(arr)
 	array_size = arr.length - 1
-	array_size.times do |i|
-		array_size.times do |j|
-			if
-				yield(arr[j], arr[j + 1]).positive?
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-			end
-		end
-	end
+	  array_size.times do |i|
+		  array_size.times do |j|
+			  if
+				  yield(arr[j], arr[j + 1]).positive?
+				    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+			  end
+		  end
+	  end
 	p arr
 end
-bubble_sort_by() do |first, second|
-	first.length - second.length
-end
+# bubble_sort_by do |first, second|
+# 	first.length - second.length
+# end
